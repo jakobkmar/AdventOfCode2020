51b1675072e7318c2cf9a167250b79219c2c3051
bluefireoly <bluefireolymp@gmail.com>
2020-12-03 20:33:17 +0100

    Update Day2.kt

--- src/main/kotlin/net/axay/adventofcode/day2/Day2.kt
+++ src/main/kotlin/net/axay/adventofcode/day2/Day2.kt
@@ -5,20 +5,16 @@ import net.axay.adventofcode.common.Day
 fun main() = Day2.run()
 
 object Day2 : Day(2) {
-    override fun part1() {
-        println("${validate { pair, char, password ->
-            password.count { it == char } in pair.first .. pair.second
-        }} passwords were valid")
-    }
+    override fun part1() =
+        validate { pair, char, password -> password.count { it == char } in pair.first .. pair.second }
 
-    override fun part2() {
-        println("${validate { pair, char, password ->
-            (password[pair.first - 1] == char) xor (password[pair.second - 1] == char)
-        }} passwords were valid")
-    }
+    override fun part2() =
+        validate { pair, char, password -> (password[pair.first - 1] == char) xor (password[pair.second - 1] == char) }
 
     private fun validate(validator: (pair: Pair<Int, Int>, char: Char, password: String) -> Boolean) =
-        inputLines.count { line -> line.split(' ').let { checkPassword(it[0], it[1], it[2], validator) } }
+        println("${
+            inputLines.count { line -> line.split(' ').let { checkPassword(it[0], it[1], it[2], validator) } }
+        } passwords were valid")
 }
 
 fun checkPassword(
