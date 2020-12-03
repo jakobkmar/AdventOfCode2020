b40ed279dbc2f5a579d1f865b6c6cc07477ca90d
bluefireoly <bluefireolymp@gmail.com>
2020-12-03 20:45:17 +0100

    Update Day3.kt

--- src/main/kotlin/net/axay/adventofcode/day3/Day3.kt
+++ src/main/kotlin/net/axay/adventofcode/day3/Day3.kt
@@ -7,16 +7,14 @@ fun main() = Day3.run()
 object Day3 : Day(3) {
     private val treeGrid = TreeGrid(inputLines)
 
-    override fun part1() {
+    override fun part1() =
         println("There were ${treeGrid.countTrees(3 to 1)} trees on the slope")
-    }
 
-    override fun part2() {
+    override fun part2() =
         listOf(1 to 1, 3 to 1, 5 to 1, 7 to 1, 1 to 2)
             .map { treeGrid.countTrees(it).toLong() }
             .reduce { acc, i -> acc * i }
             .let { println("All results multiplied with each other: $it") }
-    }
 }
 
 class TreeGrid(private val serializedMap: List<String>) {
