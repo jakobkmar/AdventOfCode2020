435d2dce9ab98a7b87d11238bab2138995527556
bluefireoly <bluefireolymp@gmail.com>
2020-12-06 14:38:23 +0100

    Update Day6.kt

--- src/main/kotlin/net/axay/adventofcode/day6/Day6.kt
+++ src/main/kotlin/net/axay/adventofcode/day6/Day6.kt
@@ -7,7 +7,7 @@ fun main() = Day6.run()
 object Day6 : Day(6) {
     private val groups = inputLinesGrouped.map { it.map { line -> line.toSet() } }
 
-    override fun part1() = println(groups.map { it.flatten().toSet().size }.sum())
+    override fun part1() = groups.map { it.flatten().toSet().size }.sum()
 
-    override fun part2() = println(groups.map { it.reduce { acc, set -> acc intersect set }.size }.sum())
+    override fun part2() = groups.map { it.reduce { acc, set -> acc intersect set }.size }.sum()
 }
